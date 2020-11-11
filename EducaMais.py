3fd48191857d31f423bcda9ef0a67f266482d4ef
Gabriel Abacherli Coelho <abacherli@live.com>
2020-11-11 12:00:09 -0300

    Cenario, personagem e organizacao das classes

---
+++ EducaMais.py
@@ -0,0 +1,57 @@
+import pygame, sys
+
+from Model.Boy import Boy
+
+pygame.init()
+
+altura = 762
+largura = 1824
+
+window = pygame.display.set_mode((largura, altura))
+tempo = pygame.time.Clock()
+
+background_image = pygame.image.load('Misc/Background/yellow.jpg').convert_alpha()
+background = pygame.transform.scale(background_image, (largura, altura))
+pygame.display.set_caption("EducaMais")
+
+boy = Boy()
+herois = pygame.sprite.Group(boy)
+
+while True:
+
+    # window.blit(background, (0, 0))
+    window.blit(background, background.get_rect(center=window.get_rect().center))
+
+    herois.draw(window)
+    # window.blit(boy,(200, 490))
+
+    # Calcular regras
+    herois.update()
+
+    if boy.rect.centery > 550:
+        boy.velocidade_y = 0
+        boy.rect.centery = 550
+        tempo = pygame.time.Clock()
+
+    # Atualizar a tela
+    pygame.display.update()
+    # tempo.tick(80)
+
+    # processa eventos
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                boy.move_direita()
+            if event.key == pygame.K_LEFT:
+                boy.move_esquerda()
+            if event.key == pygame.K_SPACE:
+                boy.pular()
+        if event.type == pygame.KEYUP:
+            if event.key in [pygame.K_LEFT, pygame.K_RIGHT]:
+                boy.para_horizontal()
+            if event.key in [pygame.K_SPACE]:
+                boy.para_vertical()
---
+++ Model/Boy.py
@@ -0,0 +1,47 @@
+import pygame
+
+
+class Boy(pygame.sprite.Sprite):
+    def __init__(self):
+        init_character = pygame.image.load("Misc/Characters/looking_right.png")
+        pygame.sprite.Sprite.__init__(self)
+        self.vl_x = 1
+        self.vl_y = 3
+        boy = init_character
+        print(self.vl_x, self.vl_y)
+        self.image = pygame.transform.scale(boy, (85, 155))
+        self.rect = pygame.Rect((10, 240), (85, 155))
+        self.velocidade_x = 0
+        self.velocidade_y = 0
+        self.gravidade = 0.009
+
+    def move_esquerda(self):
+        self.image = self.scale_image("Misc/Characters/looking_left.png")
+        self.velocidade_x = -1
+
+    def move_direita(self):
+        self.image = self.scale_image("Misc/Characters/looking_right.png")
+        self.velocidade_x = 1
+        self.vl_x = 1
+        self.vl_y = 1
+
+    def pular(self):
+        self.velocidade_y = -5
+
+    def update(self, *args):
+        self.rect.centerx += self.velocidade_x
+        # gravidade do Y
+        self.velocidade_y += self.gravidade
+        self.rect.centery += self.velocidade_y
+        self.vl_x += self.vl_x
+        self.vl_y += self.vl_y
+
+    def para_horizontal(self):
+        self.velocidade_x = 0
+
+    def para_vertical(self):
+        self.velocidade_y = 0
+
+    def scale_image(self, image):
+        boy = pygame.image.load(image)
+        return pygame.transform.scale(boy, (85, 155))
