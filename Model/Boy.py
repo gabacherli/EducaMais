b448b72ac52fd787efd55987f6962b014623e4b5
RafaelSousa07 <rafael.sousa18@yahoo.com.br>
2020-11-23 19:40:58 -0300

    Criação do movimento do personagem, caminhar para esquerda e direita

--- Model/Boy.py
+++ Model/Boy.py
@@ -4,38 +4,63 @@ class Boy(pygame.sprite.Sprite):
     def __init__(self, settings):
         boy = pygame.image.load(settings.char_right)
         pygame.sprite.Sprite.__init__(self)
-
-        self.vl_x = 0
-        self.vl_y = 0
-        self.image = pygame.transform.scale(boy, settings.char_ratio)
+        self.sprites_Right = []
+        self.sprites_Left = []
+        self.is_movementing_Right = False
+        self.is_movementing_Left = False
+        self.sprites_Right.append(self.scale_image(settings.char_right, settings.char_ratio))
+        self.sprites_Right.append(self.scale_image(settings.char_walking_right1, settings.char_ratio))
+        self.sprites_Right.append(self.scale_image(settings.char_walking_right2, settings.char_ratio))
+        self.sprites_Left.append(self.scale_image(settings.char_left, settings.char_ratio))
+        self.sprites_Left.append(self.scale_image(settings.char_walking_left1, settings.char_ratio))
+        self.sprites_Left.append(self.scale_image(settings.char_walking_left2, settings.char_ratio))
+        self.current_sprite = 0
+        self.image = self.sprites_Right[self.current_sprite]
         self.rect = pygame.Rect(settings.char_starting_pos_x, settings.char_starting_pos_y)
         self.velocidade_x = 0
         self.velocidade_y = 0
         self.gravidade = 0.03
 
     def mover_para_esquerda(self, settings):
-        self.image = self.scale_image(settings.char_left, settings.char_ratio)
+        self.is_movementing_Left = True
         self.velocidade_x = -1
 
     def mover_para_direita(self, settings):
-        self.image = self.scale_image(settings.char_right, settings.char_ratio)
+        self.is_movementing_Right = True        
         self.velocidade_x = 1
-        self.vl_x = 1
-        self.vl_y = 1
 
     def pular(self):
         self.velocidade_y = -3
 
     def update(self):
         self.rect.centerx += self.velocidade_x
+
         # gravidade do Y
         self.velocidade_y += self.gravidade
         self.rect.centery += self.velocidade_y
-        self.vl_x += self.vl_x
-        self.vl_y += self.vl_y
+
+        # Caminhar para a direita
+        if self.is_movementing_Right == True:
+            self.current_sprite += 0.03
+            if self.current_sprite >= len(self.sprites_Right):
+                self.current_sprite = 0
+            self.image = self.sprites_Right[int(self.current_sprite)]    
+
+        # Caminhar para a esquerda
+        if self.is_movementing_Left == True:
+            self.current_sprite += 0.03
+            if self.current_sprite >= len(self.sprites_Left):
+                self.current_sprite = 0
+            self.image = self.sprites_Left[int(self.current_sprite)] 
 
     def parar_movimento_horizontal(self):
         self.velocidade_x = 0
+        if self.is_movementing_Right == True:
+            self.is_movementing_Right = False
+            self.image = self.sprites_Right[int(0)] 
+        elif self.is_movementing_Left == True:
+            self.is_movementing_Left = False
+            self.image = self.sprites_Left[int(0)] 
 
     def parar_movimento_vertical(self):
         self.velocidade_y = 0
